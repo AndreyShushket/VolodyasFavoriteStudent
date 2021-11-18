2e6e962cbd2436f780c77bb5f86b68c006a6b8a5
Andrey Shushket <jonni6690@gmail.com>
2021-11-19 00:25:05 +0300

    Add class task

---
+++ Class Homework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Class_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square greenSquare = new Square(5, "Green");
+            Square blueSquare = new Square(2, "Blue");
+            Square lightblueSquare = new Square(15, "Lightblue");
+
+            greenSquare.SquareInfo();
+            blueSquare.SquareInfo();
+            lightblueSquare.SquareInfo();
+
+            greenSquare.lenght = greenSquare.lenght * 3;
+
+            greenSquare.SquareInfo();
+            blueSquare.SquareInfo();
+            lightblueSquare.SquareInfo();
+        }
+    }
+}
